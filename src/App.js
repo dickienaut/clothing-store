be18c09653bec49dfe602911780a0b583315b538
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-02-25 17:53:14 -0500

    Add signin with google via firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx'
 import SigninAndUp from './pages/signinandup/signinandup.component.jsx'
+import { auth } from './firebase/firebase.utils.js'
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SigninAndUp} />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    console.log(this.state.currentUser)
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SigninAndUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase/firebase.utils.js'
 import './header.styles.scss'
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -11,7 +12,16 @@ const Header = () => {
       <div className='options'>
         <Link className='option' to='/shop'>Shop</Link>
         <Link className='option' to='/contact'>Contact</Link>
-        <Link className='option' to='/signin'>Sign In</Link>
+
+        {
+          currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>Sign Out</div>
+          )
+          :
+          (
+          <Link className='option' to='/signin'>Sign In</Link>
+          )
+        }
       </div>
     </div>
   );
